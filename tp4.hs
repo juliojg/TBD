53c2d192c313d6aec44db07926f674c188920c8b
juliojg <julioguella@hotmail.com>
2017-11-02 23:06:54 -0300

    implementado aumentatividad

--- tp4.hs
+++ tp4.hs
@@ -20,13 +20,19 @@ powerSet s = case Data.Set.null s of True  -> Data.Set.singleton empty
 -- powerset (x:xs) = xss ++ map (x:) xss
 --                 where xss = powerset xs
 
--- Intento de implementar aumentatividad
---aument :: Set Char -> Set (Set Char, Set Char) ->  Set (Set Char, Set Char)
---aument s f= 
+-- Aumentatividad (agrega los elementos originales de la relacion, no deberia importar porque creo que se va a unir)
+aument :: Set Char -> Set (Set Char, Set Char) ->  Set (Set Char, Set Char)
+aument r f = Data.Set.foldl union empty (Data.Set.map (\x -> aumentSet r x) f) 
 
---aumentSet :: Set Char -> (Set Char, Set Char) -> Set (Set Char, Set Char)
---aumentSet r f = let p = powerSet r 
-              --  in Data.Set.map (\x)  
+aumentSet :: Set Char -> (Set Char, Set Char) -> Set (Set Char, Set Char)
+aumentSet r s = let p = powerSet r 
+                in  (Data.Set.map (\x -> aumentSet' x s) p)  
+
+-- not (isSubsetOf s x || isSubsetOf s x || isSubsetOf x s || isSubsetOf x s)
+
+--es necesario que sean disjuntos???
+aumentSet' :: Set Char -> (Set Char, Set Char) -> (Set Char, Set Char)
+aumentSet' s (x, y) = if (Data.Set.size (intersection s x) == 0 && Data.Set.size (intersection s y) == 0) then (union s x, union s y) else (x, y)
 
 --Devuelve, si es que la hay, la relacion de transitividad entre dos dependencias 
 transSet :: (Set Char, Set Char) -> (Set Char, Set Char) -> (Set Char, Set Char)
@@ -40,5 +46,8 @@ trans' s ss = delete (empty, empty) (Data.Set.map (\x -> transSet s x) ss)
 trans :: Set (Set Char, Set Char) -> Set (Set Char, Set Char) -> Set (Set Char, Set Char)
 trans ss1 ss2 = Data.Set.foldl union empty (Data.Set.map (\x -> trans' x ss2) ss1)
 
-  
+ 
+--Ejemplo: aument (fromList ['1','2','3']) (fromList [(fromList ['1'], fromList ['2']) , (fromList ['2'], fromList ['3']) ])
+
+
 
